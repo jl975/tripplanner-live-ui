8bd1eb9e7e27595a2f31335871db08df6b9c42dd
jl975 <jlee6891@gmail.com>
2015-08-21 09:51:31 -0400

    fix all non-map functunctionality

--- public/live.js
+++ public/live.js
@@ -1,19 +1,16 @@
+var days = [null, {
+		hotelArray: [],
+		restaurantArray: [],
+		activityArray: []
+	}];
+
+var currentDay = 1;
+var numDays = 1;
+var types = ['hotel', 'restaurant', 'activity'];
+var today = days[currentDay];
+
+
 $(document).ready(function(){
-
-	var days = {
-		'1': {
-			hotelArray: [],
-			restaurantArray: [],
-			activityArray: []
-		},
-	};
-
-	var types = ['hotel', 'restaurant', 'activity'];
-
-	var currentDay = '1';
-	var numDays = 1;
-
-	var today = days[currentDay];
 
 	$('.add').on('click',function(){
 		var dropdown = $(this).closest('div').find('select');
@@ -24,31 +21,31 @@ $(document).ready(function(){
 		if (!(today[type+'Array'].some(function(a) {return a == value}))) {
 			if (type == 'hotel' && today.hotelArray.length > 0) return;
 			var html = '';
-			html += '<li class="'+type+'Array'+'">'+value;
+			html += '<li class="'+type+'Array'+'"><span>'+value+'</span>';
 			html += '<button class="btn btn-xs btn-danger remove btn-circle pull-right">x</button></li>';
 			$('#my-itinerary').find('.'+type).append(html);
 			today[type+'Array'].push(value);
 		}
-		new google.maps.LatLngBounds.Builder(all)
-
-		console.log(days);
+		//new google.maps.LatLngBounds.Builder(all)
 	});
 
-	// remove
+
+	// remove itinerary items
 	$('#my-itinerary').on('click', 'button', function(event) {
 		var item = $(this).closest('li');
-		var value = item.text();
+		var value = item.find('span').text();
 		var type = item.attr('class');
 		var typeArray = today[type];
 		typeArray.splice(typeArray.indexOf(value), 1);
 		item.remove();
 	});
 
+
 	$('#add-day').on('click', function() {
 		numDays++;
 		$(this).before('<button class="btn btn-circle day-btn">'+numDays+'</button>');
-		if (!days[String(numDays)]) {
-			days[String(numDays)] = {
+		if (!days[numDays]) {
+			days[numDays] = {
 				hotelArray: [],
 				restaurantArray: [],
 				activityArray: []
@@ -56,15 +53,37 @@ $(document).ready(function(){
 		}
 	})
 
+
+	$('#remove-day').on('click', function() {
+		days.splice(currentDay, 1);
+		$('.day-buttons').children().last().prev().remove();
+		numDays--;
+
+		// automatically go to the new day that now has the number of the one
+		// that was just removed
+		// if the last day was removed, go to the new last day
+		if (currentDay > numDays) currentDay = numDays;
+		var newCurrentDay = $('button:contains("'+currentDay+'")');
+		newCurrentDay.addClass('current-day');
+
+		repopulate();
+	});
+
+
 	$('.day-buttons').on('click', 'button', function() {
 		if ($(this).text() == '+') return;
-		currentDay = $(this).text();
+		currentDay = +$(this).text();
 		$('.current-day').removeClass('current-day');
 		$(this).addClass('current-day');
 
-		today = days[currentDay];
+		repopulate();
+	})
 
-		console.log(currentDay, today);
+
+	function repopulate() {
+		today = days[currentDay];
+		$('#day-title-text').text('Day '+currentDay);
+
 		$('.list-group.hotel').empty();
 		$('.list-group.restaurant').empty();
 		$('.list-group.activity').empty();
@@ -79,10 +98,7 @@ $(document).ready(function(){
 				})
 			}
 		});
-
-
-
-	})
+	}
 
 
 
